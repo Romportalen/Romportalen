a4b30f7fe3323d6e7b920112e7620f519d2f4a15
AudunVN <audun@nytroe.net>
2015-09-14 18:20:19 +0200

    Syntax/spacing cleanup

--- res/js/main.js
+++ res/js/main.js
@@ -1,12 +1,12 @@
 // inview plugin: add inviewTrigger class to elements it should look for
 $(document).ready(function(){
-    $(".inviewTrigger").bind('inview', function (event, visible) {
-      if (visible == true) {
-        // element is visible
-       $(this).addClass('inview');
-      } else {
-        // element is not visible
-         $(this).removeClass('inview');
-      }
+    $(".inviewTrigger").bind("inview", function (event, visible) {
+        if (visible == true) {
+            // element is visible
+             $(this).addClass("inview");
+        } else {
+            // element is not visible
+            $(this).removeClass('inview');
+        }
     });
 });
