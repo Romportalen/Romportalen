f94ecbf04df3a49f9a5aea15d038fe82e0103cee
nagasjel <moutzebe@gmail.com>
2015-09-14 19:19:54 +0200

    Update main.js

--- res/js/main.js
+++ res/js/main.js
@@ -10,3 +10,24 @@ $(document).ready(function(){
         }
     });
 });
+
+(function ($) {
+    $(document).ready(function(){
+        // hide .navbar first
+	    $(".navbar").hide();
+	    // fade in .navbar
+	    $(function () {
+	       
+	    $(window).scroll(function () {
+
+              // set distance user needs to scroll before we start fadeIn
+              if ($(this).scrollTop() > 950) {
+	                $('.navbar').fadeIn();
+	            } else {
+	                $('.navbar').fadeOut();
+	            }
+	        });
+	    });
+
+	});
+	  }(jQuery));
