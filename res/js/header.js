2c5941a682a12f9ef8b01e13e49ae4a36fbc8331
Space <audun@nytroe.net>
2016-01-18 12:03:04 +0100

    added frontpage tilenav img animation classes

--- res/js/header.js
+++ res/js/header.js
@@ -2,27 +2,27 @@
 $(document).ready(function(){
 	document.querySelector(".frontPageContainer").style.top = "0px";
 	document.querySelector(".frontPageContainer .tileNav").style.height = $(window).height()-$(window).scrollTop()+"px";
-	$(".frontPageContainer .tile-image").show();
+	$(".frontPageContainer .tile-image").removeClass("hideImg");
 	$(window).scroll(function() {
 		if ($(window).scrollTop() == 0) {
 			document.querySelector(".frontPageContainer").style.top = "0px";
 			document.querySelector(".frontPageContainer .tileNav").style.height = $(window).height()-$(window).scrollTop()+"px";
-			$(".frontPageContainer .tile-image").show();
+			$(".frontPageContainer .tile-image").removeClass("hideImg");
 			console.log("scrolled to top");
 		} else if ($(window).scrollTop() < $(".frontPageContainer").height()/10*5 ) {
 			document.querySelector(".frontPageContainer").style.top = -$(window).scrollTop()+"px";
 			document.querySelector(".frontPageContainer .tileNav").style.height = $(window).height()-$(window).scrollTop()+"px";
-			$(".frontPageContainer .tile-image").show();
+			$(".frontPageContainer .tile-image").removeClass("hideImg");
 			console.log("transitioning start");
 		} else if ($(window).scrollTop() < $(".frontPageContainer").height()-$(".frontPageContainer ul li.preStudentLink a h3").height() ) {
 			document.querySelector(".frontPageContainer").style.top = -$(window).scrollTop()+"px";
 			document.querySelector(".frontPageContainer .tileNav").style.height = $(window).height()-$(window).scrollTop()+"px";
-			$(".frontPageContainer .tile-image").hide();
+			$(".frontPageContainer .tile-image").addClass("hideImg");
 			console.log("transitioning mid");
 		} else {
 			document.querySelector(".frontPageContainer").style.top = -($(".frontPageContainer").height()-$(".frontPageContainer ul li a h3").height())+"px";
 			document.querySelector(".frontPageContainer .tileNav").style.height = $(".frontPageContainer ul li.preStudentLink a h3").height()+"px";
-			$(".frontPageContainer .tile-image").hide();
+			$(".frontPageContainer .tile-image").addClass("hideImg");
 			console.log("navbar state");
 		}
 		var navHeight = $(".frontPageContainer ul li a h3").height()+2;
