381cf23351a8ca6dedbccde5435fd0c3043be63f
Space <audun@nytroe.net>
2016-01-31 00:11:24 +0100

    progress bar added, navbar improved, bugfixes

--- res/js/header.js
+++ res/js/header.js
@@ -1,6 +1,6 @@
 // fancy header function
 
-var lastScrollPosition = 1;
+var lastScrollPosition = -1;
 
 	function updateHeaderPosition() {
 		window.requestAnimationFrame(updateHeaderPosition);
@@ -8,6 +8,21 @@ var lastScrollPosition = 1;
 			lastScrollPosition = $(window).scrollTop();
 			document.querySelector(".frontPageContainer").style.top = "0px";
 			document.querySelector(".frontPageContainer .tileNav").style.height = $(window).height()-$(window).scrollTop()+"px";
+
+		    /* title animation rules start */
+			if ($(window).scrollTop() < $(".frontPageContainer").height()/10) {
+				document.querySelector(".titleContainer").style.top = "10vh";
+				document.querySelector(".titleContainer").style.bottom = "auto";
+			} else if ($(window).scrollTop() < $(".frontPageContainer").height()-$(".frontPageContainer .titleContainer h1").outerHeight()-$(".frontPageContainer ul li a h3").height()) {
+				document.querySelector(".titleContainer").style.top = $(window).scrollTop()+"px";
+				document.querySelector(".titleContainer").style.bottom = "auto";
+			} else {
+					document.querySelector(".titleContainer").style.top = "auto";
+					document.querySelector(".titleContainer").style.bottom = $(".frontPageContainer .titleContainer h1").outerHeight()+$(".frontPageContainer ul li a h3").height()+"px";
+			}
+			/* title animation rules end */
+
+			/* link animation rules start */
 			if ($(window).scrollTop() == 0) {
 				document.querySelector(".frontPageContainer").style.top = "0px";
 				document.querySelector(".frontPageContainer .tileNav").style.height = $(window).height()-$(window).scrollTop()+"px";
@@ -18,19 +33,22 @@ var lastScrollPosition = 1;
 				document.querySelector(".frontPageContainer .tileNav").style.height = $(window).height()-$(window).scrollTop()+"px";
 				$(".frontPageContainer .tile-image").removeClass("hideImg");
 				$(".frontPageContainer").removeClass("navbarState");
-			} else if ($(window).scrollTop() < $(".frontPageContainer").height()-$(".frontPageContainer ul li.preStudentLink a h3").height() ) {
+			} else if ($(window).scrollTop() < $(".frontPageContainer").height()-$(".frontPageContainer ul li.preStudentLink a h3").height()-$(".frontPageContainer .titleContainer h1").outerHeight() ) {
 				document.querySelector(".frontPageContainer").style.top = -$(window).scrollTop()+"px";
 				document.querySelector(".frontPageContainer .tileNav").style.height = $(window).height()-$(window).scrollTop()+"px";
 				$(".frontPageContainer .tile-image").addClass("hideImg");
 				$(".frontPageContainer").addClass("navbarState");
 			} else {
-				document.querySelector(".frontPageContainer").style.top = -($(".frontPageContainer").height()-$(".frontPageContainer ul li a h3").height())+"px";
+				document.querySelector(".frontPageContainer").style.top = -($(".frontPageContainer").height()-$(".frontPageContainer ul li a h3").height())+$(".frontPageContainer .titleContainer h1").outerHeight()+"px";
 				document.querySelector(".frontPageContainer .tileNav").style.height = $(".frontPageContainer ul li.preStudentLink a h3").height()+"px";
 				$(".frontPageContainer .tile-image").addClass("hideImg");
 				$(".frontPageContainer").addClass("navbarState");
 			}
+			/* link animation rules end */
+
 			var navHeight = $(".frontPageContainer ul li a h3").height()+2;
 			if($(window).scrollTop() > $("#jobb").offset().top-navHeight) {
+				document.querySelector(".progressBar").style.width = 75+(($(window).scrollTop()-$("#jobb").offset().top)/($(document).height()-$(window).height()-$("#jobb").offset().top))*25+"%";
 				$(".postStudentLink.tileButton h3").addClass("currentSection");
 				if (document.origin != "null") {
 					history.replaceState("Romportalen.no - Student", "Romportalen.no - Student", "#jobb");
@@ -39,6 +57,7 @@ var lastScrollPosition = 1;
 				$(".preStudentLink.tileButton h3").removeClass("currentSection");
 				$(".studentLink.tileButton h3").removeClass("currentSection");
 			} else if($(window).scrollTop() > $("#student").offset().top-navHeight) {
+				document.querySelector(".progressBar").style.width = 50+(($(window).scrollTop()-$("#student").offset().top)/($("#jobb").offset().top-$("#student").offset().top))*25+"%";
 				$(".studentLink.tileButton h3").addClass("currentSection");
 				if (document.origin != "null") {
 					history.replaceState("Romportalen.no - Student", "Romportalen.no - Student", "#student");
@@ -47,6 +66,7 @@ var lastScrollPosition = 1;
 				$(".preStudentLink.tileButton h3").removeClass("currentSection");
 				$(".postStudentLink.tileButton h3").removeClass("currentSection");
 			} else if($(window).scrollTop() > $("#hoeyereUtdanning").offset().top-navHeight) {
+				document.querySelector(".progressBar").style.width = 25+(($(window).scrollTop()-$("#hoeyereUtdanning").offset().top)/($("#student").offset().top-$("#hoeyereUtdanning").offset().top))*25+"%";
 				$(".preStudentLink.tileButton h3").addClass("currentSection");
 				if (document.origin != "null") {
 					history.replaceState("Romportalen.no - Høyere utdanning", "Romportalen.no - Høyere utdanning", "#hoeyereUtdanning");
@@ -55,6 +75,7 @@ var lastScrollPosition = 1;
 				$(".studentLink.tileButton h3").removeClass("currentSection");
 				$(".postStudentLink.tileButton h3").removeClass("currentSection");
 			} else if($(window).scrollTop() > $("#generellInfo").offset().top-navHeight) {
+				document.querySelector(".progressBar").style.width = 0+(($(window).scrollTop()-$("#generellInfo").offset().top)/($("#hoeyereUtdanning").offset().top-$("#generellInfo").offset().top))*25+"%";
 				$(".generalLink.tileButton h3").addClass("currentSection");
 				if (document.origin != "null") {
 					history.replaceState("Romportalen.no - Generelt", "Romportalen.no - Generelt", "#generellInfo");
@@ -63,6 +84,7 @@ var lastScrollPosition = 1;
 				$(".studentLink.tileButton h3").removeClass("currentSection");
 				$(".postStudentLink.tileButton h3").removeClass("currentSection");
 			} else {
+				document.querySelector(".progressBar").style.width = "0%";
 				if (document.origin != "null") {
 					history.replaceState("Romportalen.no", "Romportalen.no", ".");
 				}
@@ -76,4 +98,7 @@ var lastScrollPosition = 1;
 
 $(document).ready(function(){
 	updateHeaderPosition();
+    $("a").click(function() {
+		setTimeout(function(){lastScrollPosition = -5;}, 50);
+    });
 });
